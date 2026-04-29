db60642b42b7ff1758220a0e51f2a8bc02a5915d
okliland-kodeloftet <okliland@hotmail.com>
2026-04-29 10:32:19 +0200

    hh

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log('Hello!');
+
+
+
+const favorittAnimeFilmer = [
+    {
+        tittel: "Akira",
+        årstall: 1988,
+        regissør: "Katsuhiro Otomo"
+    },
+
+    {
+        tittel: "Perfect Blue",
+        årstall: 1997,
+        regissør: "Satoshi Kon"
+    },
+
+    {
+        tittel: "Ghost in the Shell",
+        årstall: 1995,
+        regissør: "Mamoru Oshii"
+    },
+
+    {
+        tittel: "Paprika",
+        årstall: 2006,
+        regissør: "Satoshi Kon"
+    }
+];
+
+console.log(favorittAnimeFilmer);
